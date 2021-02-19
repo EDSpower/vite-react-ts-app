ebddfb0ef5f88d50f723d848e1cda1ec2d9ce61c
EDSpower <766782971@qq.com>
2021-02-19 17:26:13 +0800

    test

--- src/pages/page1/index.tsx
+++ src/pages/page1/index.tsx
@@ -1,13 +1,5 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 
-const Page1: React.FC = () => {
-  const history = useHistory();
-  return (
-    <div onClick={() => history.push('/page2')}>
-      Page1Page1Page11Page1Page1Pa2ge1Pa4ge13Page1Page1Pa3ge1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1
-    </div>
-  );
-};
+const Page1: React.FC = () => <div>Page1</div>
 
 export default Page1;
