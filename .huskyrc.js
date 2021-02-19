4708b8cf12c2e9dc9c4debc71dde91ea177a667d
EDSpower <766782971@qq.com>
2021-02-19 17:27:54 +0800

    test

--- .huskyrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  hooks: {
-    // git commit 前的钩子
-    "pre-commit": "echo 'husky' && npm run lint",
-  },
-};
--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  'src/**/*.{js,jsx,tsx}': ['eslint --fix', 'git add'],
-  'src/**/*.{jsx,tsx,html,css,scss,sass}': ['stylelint --fix', 'git add'],
-  'src/**/*.{js,jsx,tsx,html,css,scss,sass}': ['prettier-eslint --write', 'git add'],
-};
