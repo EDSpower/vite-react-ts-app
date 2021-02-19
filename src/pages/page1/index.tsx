d6fd32cef9388951ad939e8a2ce823b60779ffc2
EDSpower <766782971@qq.com>
2021-02-19 17:22:59 +0800

    test

--- src/pages/page1/index.tsx
+++ src/pages/page1/index.tsx
@@ -5,7 +5,7 @@ const Page1: React.FC = () => {
   const history = useHistory();
   return (
     <div onClick={() => history.push('/page2')}>
-      Page1Page1Page11Page1Page1Pa2ge1Page13Page1Page1Pa3ge1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1
+      Page1Page1Page11Page1Page1Pa2ge1Pa4ge13Page1Page1Pa3ge1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1Page1
     </div>
   );
 };
